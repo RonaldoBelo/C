f9424834217f742e83e47dba438388931284a758
RonaldoBelo <162352657+RonaldoBelo@users.noreply.github.com>
2024-06-29 23:17:17 -0300

    Add files via upload

---
+++ String_Ex4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    char str[100], ap[100];
+    puts("Digite algo: ");
+    fgets(str, sizeof str, stdin);
+    for(int i=0; i<strlen(str); i++){
+        ap[i]=
+    }
+
+    return 0;
+}
